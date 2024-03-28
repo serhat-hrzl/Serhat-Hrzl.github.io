c73396aafda2567f89c2c6187521c141e2eb5541
Kush Kashyap <kashyap.kush@gmail.com>
2024-03-28 13:52:50 +0100

    Update main.js

--- main.js
+++ main.js
@@ -410,13 +410,13 @@ const parseMetadata = (metadata) => {
 
         var statusColor;
         switch (api.value(9)) {
-          case "Green":
+          case "GREEN":
             statusColor = "#0e8f33";
             break;
-          case "Red":
+          case "RED":
             statusColor = "#941403";
             break;
-          case "Grey":
+          case "GREY":
             statusColor = "#a3a3a3";
             break;
         }
