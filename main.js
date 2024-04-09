42d821491343d0b13f3bfcb5b0f0855b95644850
Narendragowdakv <84325292+Narendragowdakv@users.noreply.github.com>
2024-04-09 09:46:10 +0200

    Update main.js

--- main.js
+++ main.js
@@ -301,7 +301,7 @@ const parseMetadata = (metadata) => {
             position: "top",
             min: function (value) {
               let d = new Date();
-              d.setDate(d.getDate() - 3);
+              d.setDate(d.getDate() - 4);
               d.setHours(0, 0, 0, 0);
               return d;
             },
