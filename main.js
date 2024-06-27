f56c00747e114ebe11de4f663c947c96ea37ccc8
Narendragowdakv <84325292+Narendragowdakv@users.noreply.github.com>
2024-06-27 12:55:23 +0200

    Update main.js

--- main.js
+++ main.js
@@ -111,7 +111,7 @@ const parseMetadata = (metadata) => {
        while (i<length.holidays) 
        {
          (console.log(holidays[i])
-          i++;
+          i+1;
        }  
         row[5] = row[8];
       });
