71402ef758b8392a15f7818f75d5281dbcf77919
Narendragowdakv <84325292+Narendragowdakv@users.noreply.github.com>
2024-07-15 16:26:00 +0200

    Update main.js

--- main.js
+++ main.js
@@ -169,10 +169,10 @@ const parseMetadata = (metadata) => {
         } else 
         { row[DIM_BUFFER] = parseFloat(row[DIM_BUFFER]) - holidaysCount * 24 * 60;}
 
-        if (nowTime <== row[DIM_TIME_START]){
-         row[DIM_BUFFER] == 0;
+        if (nowTime <= row[DIM_TIME_START]){
+         row[DIM_BUFFER] = 0;
         } else 
-        { row[DIM_BUFFER] == row[DIM_BUFFER] }
+        { row[DIM_BUFFER] = row[DIM_BUFFER] }
       });
 
       /*-------------------------------------Chart related customizations---------------------------------------*/
